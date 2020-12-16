701a39769f5aa5691b6c74a59d7dd9dedcffcaee
TryMe <TryMe@DESKTOP-ETLRID9>
2020-12-17 01:05:14 +0530

    File read and capitalize it.

---
+++ CapitalizedFileReader/src/org/crazycoder/filereader/Application.java
@@ -0,0 +1,23 @@
+package org.crazycoder.filereader;
+
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.List;
+
+public class Application {
+
+	public static void main(String[] args) {
+		try {
+			CapitalizeFileReader capitalizeFileReader = new CapitalizeFileReader();
+			List<String> capitalLines = capitalizeFileReader.readFileInCapital("CheckFile.txt");
+			for (String line : capitalLines) {
+				System.out.println(line);
+			}
+		} catch (FileNotFoundException fileNotFoundException) {
+			System.out.println("File not found exception " + fileNotFoundException.toString());
+		} catch (IOException ioException) {
+			System.out.println("IO exception " + ioException.toString());
+		}
+	}
+
+}
