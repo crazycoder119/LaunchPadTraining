1057464e2b4da273b5c46ce16d898d5748fb14dd
TryMe <TryMe@DESKTOP-ETLRID9>
2020-12-17 08:08:27 +0530

    comit changes

--- Number Reverse Projeect/src/org/crazycoder/numberreverser/Application.java
+++ Number Reverse Projeect/src/org/crazycoder/numberreverser/Application.java
@@ -5,7 +5,7 @@ import java.math.BigInteger;
 import java.util.Stack;
 
 public class Application {
-	public static void main(String[] args) throws IOException {
+	public static void main(String[] args)  {
 
 		try {
 			Stack<BigInteger> bigIntegerStack = new Stack<BigInteger>();
